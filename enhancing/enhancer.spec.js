bff023312e8ef14c25844fa80a8001b5402715fb
RaneWallin <rane@ranewallin.com>
2019-06-07 11:13:51 -0700

    scaffold in test suite and base test objects

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+const baseSword = {
+    name: "A Wooden Sword",
+    durability: 5,
+    enhancement: 10
+};
+
+const baseBook = {
+    name: "An Old Book",
+    durability: 5,
+    enhancement: 10
+};
+
+const baseShield = {
+    name: "An Iron Shield",
+    durability: 15,
+    enhancement: 2
+};
+
+describe('Testing enhancer.js', () => {
+
+})
